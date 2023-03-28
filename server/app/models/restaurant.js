77d96072f68fb5c7a51897c931524d7286dd0eec
amaghami <maghami.a1987@gmail.com>
2023-03-28 11:41:45 +0330

    feat: create restaurant model

---
+++ server/app/models/restaurant.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose")
+
+const commentSchema = new mongoose.Schema({
+  user: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  score: {
+    type: Number
+  },
+})
+const foodSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  descriptaion: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  pic: {
+    type: String,
+    required: true
+  },
+  comments: [commentSchema]
+})
+const restaurantSchema = new mongoose.Schema({
+  name: {
+    type: String, 
+    required: true
+  },
+  descriptaion:{
+    
+    type: String,
+    required: true
+  },
+  score:{
+    type: String,
+    required: true
+  },
+  score:{
+    type: Number,
+    default: 0
+  },
+  address:{
+    type: String
+  },
+  pic:{
+    type: String
+  },
+  comments:[commentSchema],
+  menu: [foodSchema]
+})
+
+module.exports =  mongoose.model("restaurant",restaurantSchema)
+
